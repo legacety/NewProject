7cbc29accf315f391b28de902c2d12f84e7066a4
legacety <legacety@gmail.com>
2026-04-27 18:33:47 +0300

    Update Tprojecty.lua

--- Tprojecty.lua
+++ Tprojecty.lua
@@ -1,4 +1,4 @@
-script_version('5')
+script_version('6')
 
 local encoding = require('encoding')
 encoding.default = 'CP1251'
@@ -10,37 +10,25 @@ function checkUpdate()
     
     local response = requests.get(url)
     if response.status_code == 200 then
-        -- Парсим JSON (считаем, что в JSON может быть кириллица в UTF-8)
         local data = decodeJson(u8:decode(response.text))
         local lastver = tostring(data['last'])
+        local curver = thisScript().version
         
-        if lastver ~= thisScript().version then
-            sampAddChatMessage('{33AAFF}[Updater]: {FFFFFF}Обнаружено обновление. Загрузка...', -1)
-            
-            -- Вместо downloadUrlToFile используем requests, чтобы перекодировать содержимое
+        if lastver ~= curver then
             local res = requests.get(data['url'])
             if res.status_code == 200 then
-                -- Переводим весь скачанный код скрипта из UTF-8 в Windows-1251
                 local script_content = u8:decode(res.text)
-                
-                -- Сохраняем файл вручную в кодировке 1251
                 local f = io.open(thisScript().path, 'wb')
                 if f then
                     f:write(script_content)
                     f:close()
-                    sampAddChatMessage('{33AAFF}[Updater]: {FFFFFF}Скрипт обновлен (CP1251), перезагрузка...', -1)
+                    sampAddChatMessage('{33AAFF}[Updater]: {FFFFFF}Скрипт обновлен с версии {FF6347}' .. curver .. ' {FFFFFF}до {32CD32}' .. lastver .. '{FFFFFF}. Перезагрузка...', -1)
                     thisScript():reload()
-                else
-                    sampAddChatMessage('{33AAFF}[Updater]: {FFFFFF}Ошибка записи файла.', -1)
                 end
-            else
-                sampAddChatMessage('{33AAFF}[Updater]: {FFFFFF}Ошибка при скачивании файла обновления.', -1)
             end
         else
-            sampAddChatMessage('{33AAFF}[Updater]: {FFFFFF}У вас актуальная версия.', -1)
+            sampAddChatMessage('{33AAFF}[Updater]: {FFFFFF}У вас актуальная версия {32CD32}v' .. curver .. '{FFFFFF}.', -1)
         end
-    else
-        sampAddChatMessage('{33AAFF}[Updater]: {FFFFFF}Ошибка проверки обновлений.', -1)
     end
 end
 
