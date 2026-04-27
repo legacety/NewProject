9c24b769dce1f699359eb8d2319e8f3a6605ce87
legacety <legacety@gmail.com>
2026-04-27 18:24:35 +0300

    Update Tprojecty.lua

--- Tprojecty.lua
+++ Tprojecty.lua
@@ -1,24 +1,31 @@
-script_version('3')
+script_version('4')
+
+-- Подключаем библиотеку для работы с кодировками
+local encoding = require('encoding')
+encoding.default = 'CP1251'
+local u8 = encoding.UTF8
 
 function checkUpdate()
-    -- Используем стандартный samp.events или другие зависимости, если нужно
-    -- Но для загрузки обычно достаточно встроенных функций
     local dlstatus = require('moonloader').download_status
     local url = 'https://raw.githubusercontent.com/legacety/NewProject/refs/heads/main/update.json'
     
-    -- Для проверки обновлений лучше использовать asyncHttpRequest, чтобы не «фризить» игру
-    -- Но раз в коде requests (библиотека), оставим логику схожей, добавив обработку кодировки текста
     local requests = require('requests')
     
+    -- Выполняем запрос
     local response = requests.get(url)
+    
     if response.status_code == 200 then
-        local data = decodeJson(response.text)
+        -- Конвертируем полученный текст из UTF-8 (стандарт GitHub) в Windows-1251
+        local json_text = u8:decode(response.text)
+        local data = decodeJson(json_text)
+        
         local lastver = tostring(data['last'])
         
         -- Сравнение версии
         if lastver ~= thisScript().version then
             sampAddChatMessage('{33AAFF}[Updater]: {FFFFFF}Обнаружено обновление. Загрузка...', -1)
             
+            -- Скачивание файла
             downloadUrlToFile(data['url'], thisScript().path, function(id, status, p1, p2)
                 if status == dlstatus.STATUSEX_ENDDOWNLOAD then
                     sampAddChatMessage('{33AAFF}[Updater]: {FFFFFF}Скрипт обновлен, перезагрузка...', -1)
