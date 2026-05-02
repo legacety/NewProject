d4de82f460f72be8d3c49c6b72b165fbe803fccd
legacety <legacety@gmail.com>
2026-05-02 16:46:09 +0300

    Update Tprojecty.lua

--- Tprojecty.lua
+++ Tprojecty.lua
@@ -1,39 +1,40 @@
-script_version('7')
+script_version('8')
 
 local encoding = require('encoding')
 encoding.default = 'CP1251'
 local u8 = encoding.UTF8
+local requests = require('requests')
 
 function checkUpdate()
     local url = 'https://raw.githubusercontent.com/legacety/NewProject/refs/heads/main/update.json'
-    local requests = require('requests')
     
     local response = requests.get(url)
     if response.status_code == 200 then
         local data = decodeJson(u8:decode(response.text))
-        local lastver = tostring(data['last'])
-        local curver = thisScript().version
         
-        if lastver ~= curver then
+        local Ver = thisScript().version
+        local newVer = tostring(data['version'])
+        
+        if newVer ~= Ver then
             local res = requests.get(data['url'])
             if res.status_code == 200 then
-                local script_content = u8:decode(res.text)
-                local f = io.open(thisScript().path, 'wb')
-                if f then
-                    f:write(script_content)
-                    f:close()
-                    sampAddChatMessage('{00FFFF}[Updater]: {FFFFFF}Скрипт обновлен с версии {00FFFF}' .. curver .. ' {FFFFFF}до {00FFFF}' .. lastver, -1)
+                local file = io.open(thisScript().path, 'wb')
+                if file then
+                    file:write(u8:decode(res.text))
+                    file:close()
+                    
+                    sampAddChatMessage(string.format('{00FFFF}[Updater]: {FFFFFF}Обновлено: {00FFFF}%s {FFFFFF}-> {00FFFF}%s', Ver, newVer), -1)
                     thisScript():reload()
                 end
             end
         else
-            sampAddChatMessage('{00FFFF}[Updater]: {FFFFFF}У вас актуальная версия {00FFFF}' .. curver .. '{FFFFFF}.', -1)
+            sampAddChatMessage('{00FFFF}[Updater]: {FFFFFF}У вас актуальная версия: {00FFFF}' .. Ver, -1)
         end
     end
 end
 
 function main()
-    if not isSampLoaded() or not isSampAvailable() then return end
+    while not isSampAvailable() do wait(100) end
     checkUpdate()
     wait(-1)
 end
